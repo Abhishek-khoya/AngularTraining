06aba8efe0eb48d782a2f0a0f1e1f984545a3152
Abhishek Khoya <abhishekkhoya786@gmail.com>
2022-12-14 21:41:34 +0530

    Delete AngularTraining directory

--- AngularTraining/assignmentstypescript/que1.js
+++
@@ -1,9 +0,0 @@
-var nNumberOfArgs = function () {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i] = arguments[_i];
-    }
-    var sortedArray = args.sort(function (a, b) { return a.length - b.length; });
-    console.log(sortedArray);
-};
-nNumberOfArgs("good", "bad", "lal", "keel", "jai", "ujjain");
--- AngularTraining/assignmentstypescript/que1.ts
+++
@@ -1,5 +0,0 @@
-let nNumberOfArgs = (...args: string[]) => {
-   var sortedArray= args.sort((a,b)=>a.length-b.length);
-    console.log(sortedArray);
-};
-nNumberOfArgs("good","bad","lal","keel","jai","ujjain");
