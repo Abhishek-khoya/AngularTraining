fe1d3bc2c6dc8b3222c5d56e099d7717df049dec
Abhishek Khoya <abhishek.khoya@yash.com>
2022-12-19 11:47:31 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name:string="";
+  buttonState:boolean=false;
   title = 'WebApplicationTwo';
+  resetForm()
+  {
+    this.name="";
+  }
+
+  fullName:string ="Your Name = Abhishek Khoya";
+  text:boolean=false;
+  count:number = 0;
+  clickCount:number[] =[];
+
+  displayParagraph()
+  {
+    this.clickCount.push(++this.count);
+    this.text = !this.text;
+  }
+  BackgroundColor(i:number)
+  {
+    if(i>=5)
+    {
+      return "blue";
+    }
+    else
+    {
+      return "white";
+    }
+  }
 }
